e901943c7559dc8ab9ebd4d0539180d141b6168b
llomal <michaellobanov@MacBook-Pro-Michael.local>
2020-05-12 11:26:36 +0300

    add validator link

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -128,11 +128,18 @@
                                     {{ $td('Explorer', 'common.page-explorer') }}
                                 </a>
                             </a>
-                            <a  href="http://faq.noah-blockchain.com" target="_blank" class="menu__item u-cell">
-                                <a  class="menu__link link--hover" :to="preferredPath('coiner')"
-                                           @click.native="linkClick">
+                            <a
+                                v-if="$i18n.locale === 'jp'" :href="'https://github.com/noah-blockchain/ark-wallet/wiki/%E3%83%8E%E3%82%A2%E3%83%84%E3%83%BC%E3%83%AB%E3%81%AE%E3%82%AC%E3%82%A4%E3%83%89%E3%80%82%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC%E3%83%8E%E3%83%BC%E3%83%89%E3%82%92%E9%96%8B%E5%A7%8B%E3%81%97%E3%80%81%E3%83%90%E3%83%AA%E3%83%87%E3%83%BC%E3%82%BF%E3%83%BC%E3%81%AB%E3%81%AA%E3%82%8B%E6%96%B9%E6%B3%95'" target="_blank" class="menu__item u-cell validator-manual">
+                                <a  class="menu__link link--hover" :to="preferredPath('coiner')" @click.native="linkClick">
                                     <img class="menu__icon" src="/img/faq.svg" alt="" role="presentation">
-                                    {{ $td('Help/Faq', 'common.page-faq') }}
+                                    {{ $td('Validator manual', 'common.page-manual') }}
+                                </a>
+                            </a>
+                            <a
+                                v-else :href="'https://github.com/noah-blockchain/ark-wallet/wiki/Noah-Tools-Guide:-How-to-Launch-Masternode-and-Become-a-Validator(ENG)'" target="_blank" class="menu__item u-cell validator-manual">
+                                <a  class="menu__link link--hover" :to="preferredPath('coiner')" @click.native="linkClick">
+                                    <img class="menu__icon" src="/img/faq.svg" alt="" role="presentation">
+                                    {{ $td('Validator manual', 'common.page-manual') }}
                                 </a>
                             </a>
                         </menu>
